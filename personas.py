84d087396871d4b8df160b280a9f72d77c2804d9
nikoldayannaf-ctrl <nikoldayannaf@gmail.com>
2025-11-28 17:16:24 -0500

    Delete personas.py

--- personas.py
+++
@@ -1,8 +0,0 @@
-class Persona:
-    def _init_(self, nombre, apellido, documento):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.documento = documento
-
-    def _str_(self):
-        return f"{self.nombre} {self.apellido} - Documento: {self.documento}"
